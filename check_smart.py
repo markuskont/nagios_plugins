c549aac2e7f3f0076946825d0520e72da3db626e
Markus Kont <markuskont@gmail.com>
2014-11-01 16:17:57 +0200

    code cleanup

--- check_smart.py
+++ check_smart.py
@@ -3,7 +3,7 @@
 import sys, os, re
 
 # Simply get all SMART data for device
-def checksmart():
+def read_smart():
     cmd='sudo smartctl -a /dev/sda'
     
     return os.popen(cmd).read()
@@ -11,15 +11,12 @@ def checksmart():
 # check if device is HDD or SSD
 def disk_type(argv):
 
-    #devicetype=re.search('Rotation Rate:.+', argv)
-    #SSD=re.compile('Rotation Rate:\s*solid\s*state\s*device', re.IGNORECASE)
-    #SSD=re.search('Rotation Rate:.+')
-
     if re.search('Rotation Rate:\s*Solid\s*State\s*Device', argv):
         return "SSD"
     else:
         return "HDD"
 
+# Run health checks
 def ssd_health_check(argv):
 
     print "this is SSD"
@@ -29,7 +26,7 @@ def hdd_health_check(argv):
     print "this is HDD"
 
 def main():
-    status=checksmart()
+    status=read_smart()
 
     if disk_type(status) == "SSD":
         ssd_health_check(status)
