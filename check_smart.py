267a60a38585b2ad28e6d697dfd659e2fdda2871
Markus Kont <markuskont@gmail.com>
2014-11-01 14:37:02 +0200

    SSD identification functionality

--- check_smart.py
+++ check_smart.py
@@ -2,10 +2,15 @@
 
 import sys, os
 
+# Simply get all SMART data for device
 def checksmart():
     cmd='sudo smartctl -a /dev/sda'
     status=os.system(cmd)
     return status
+
+# check if device is HDD or SSD
+def check_disk_type():
+    print "asd"
 
 def main():
     print checksmart()
